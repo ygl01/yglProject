e52d932704208aac6cc6008e181be16c1e54ec4d
“ygl01” <yangaoling0110@163.com>
2021-03-05 17:43:04 +0800

    es搜索属性完善，面包屑之前完成

--- gmall-api/src/main/java/com/ygl/gmall/bean/PmsSearchParam.java
+++ gmall-api/src/main/java/com/ygl/gmall/bean/PmsSearchParam.java
@@ -7,7 +7,8 @@ public class PmsSearchParam implements Serializable {
 
     private String catalog3Id;
     private String keyword;
-    private List<PmsSkuAttrValue> skuAttrValueList;
+//    private List<PmsSkuAttrValue> skuAttrValueList;
+    private String[] valueId;
 
     public String getCatalog3Id() {
         return catalog3Id;
@@ -25,11 +26,11 @@ public class PmsSearchParam implements Serializable {
         this.keyword = keyword;
     }
 
-    public List<PmsSkuAttrValue> getSkuAttrValueList() {
-        return skuAttrValueList;
+    public String[] getValueId() {
+        return valueId;
     }
 
-    public void setSkuAttrValueList(List<PmsSkuAttrValue> skuAttrValueList) {
-        this.skuAttrValueList = skuAttrValueList;
+    public void setValueId(String[] valueId) {
+        this.valueId = valueId;
     }
 }
--- gmall-search-web/src/main/java/com/ygl/gmall/search/controller/SearchController.java
+++ gmall-search-web/src/main/java/com/ygl/gmall/search/controller/SearchController.java
@@ -1,13 +1,11 @@
 package com.ygl.gmall.search.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.ygl.gmall.bean.PmsBaseAttrInfo;
-import com.ygl.gmall.bean.PmsSearchParam;
-import com.ygl.gmall.bean.PmsSearchSkuInfo;
-import com.ygl.gmall.bean.PmsSkuAttrValue;
+import com.ygl.gmall.bean.*;
 import com.ygl.gmall.service.AttrService;
 import com.ygl.gmall.service.SearchService;
 import com.ygl.gmall.service.SkuService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -35,8 +34,8 @@ public class SearchController {
 
         //调用搜索服务，返回搜索服务结果
         List<PmsSearchSkuInfo> pmsSearchSkuInfoList = searchService.list(pmsSearchParam);
-        System.out.println("查询结果大小："+pmsSearchSkuInfoList.size());
-        modelMap.put("skuLsInfoList",pmsSearchSkuInfoList);
+        System.out.println("查询结果大小：" + pmsSearchSkuInfoList.size());
+        modelMap.put("skuLsInfoList", pmsSearchSkuInfoList);
 
         //抽取检索结果所包含的平台属性集合
         HashSet<String> valueSet = new HashSet<>();
@@ -50,16 +49,69 @@ public class SearchController {
         //根据valueId将属性列表查询出来
         List<PmsBaseAttrInfo> pmsBaseAttrInfos = attrService.getAttrValueListByValueId(valueSet);
         for (PmsBaseAttrInfo pmsBaseAttrInfo : pmsBaseAttrInfos) {
-            System.out.println("查询的平台属性："+pmsBaseAttrInfo.getAttrName());
+            System.out.println("查询的平台属性：" + pmsBaseAttrInfo.getAttrName());
+        }
+        modelMap.put("attrList", pmsBaseAttrInfos);
+
+        //对平台属性集合进一步处理，去掉当前条件中valueId的属性组
+        String[] delValueId = pmsSearchParam.getValueId();
+        if (delValueId != null){
+            Iterator<PmsBaseAttrInfo> iterator = pmsBaseAttrInfos.iterator();
+            while (iterator.hasNext()){
+                PmsBaseAttrInfo pmsBaseAttrInfo = iterator.next();
+                List<PmsBaseAttrValue> attrValueList = pmsBaseAttrInfo.getAttrValueList();
+                for (PmsBaseAttrValue pmsBaseAttrValue : attrValueList) {
+                    String id = pmsBaseAttrValue.getId();
+                    for (String s : delValueId) {
+                        if (s.equals(id)){
+                            //删除该属性所在的属性组
+                            iterator.remove();
+                        }
+                    }
+                }
+            }
         }
 
 
-        modelMap.put("attrList",pmsBaseAttrInfos);
+        String urlParam = getUrlParam(pmsSearchParam);
+        modelMap.put("urlParam", urlParam);
+
         return "list";
     }
 
+    private String getUrlParam(PmsSearchParam pmsSearchParam) {
+        String keyword = pmsSearchParam.getKeyword();
+        String catalog3Id = pmsSearchParam.getCatalog3Id();
+        String[] skuAttrValueList = pmsSearchParam.getValueId();
+        String urlParam = "";
+
+        if (StringUtils.isNotBlank(keyword)) {
+            if (StringUtils.isNotBlank(urlParam)){
+                urlParam = urlParam + "&";
+            }
+            urlParam = urlParam + "keyword=" + keyword;
+        }
+
+        if (StringUtils.isNotBlank(catalog3Id)) {
+            if (StringUtils.isNotBlank(urlParam)){
+                urlParam = urlParam +"&";
+            }
+            urlParam = urlParam + "catalog3Id=" + catalog3Id;
+        }
+
+        if (skuAttrValueList != null) {
+            for (String pmsSkuAttrValue : skuAttrValueList) {
+
+                urlParam = urlParam + "&valueId=" + pmsSkuAttrValue;
+            }
+        }
+
+        System.out.println("打印urlParam："+urlParam);
+        return urlParam;
+    }
+
     @GetMapping("index")
-    public String index(){
+    public String index() {
         return "/index";
     }
 }
